5f7407a837c5b013d5e330a605bff1e53f8772f8
Maryna Kyrychynska <m_kirichok@ukr.net>
2024-02-05 14:10:36 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,29 @@
+"use strict";
+function convertToNumber(salary) {
+    const number = parseFloat(salary.replace("$", "").replaceAll(",", ""));
+    return number;
+}
+const mainList = document.querySelector("ul");
+function sortList(list) {
+    [
+        ...list.querySelectorAll("li")
+    ].sort((a, b)=>{
+        const convertA = convertToNumber(a.dataset.salary);
+        const convertB = convertToNumber(b.dataset.salary);
+        return convertB - convertA;
+    }).forEach((li)=>list.appendChild(li));
+}
+function getEmploees(list) {
+    [
+        ...list.querySelectorAll("li")
+    ].map((item)=>({
+            name: item.innerText,
+            position: item.dataset.position,
+            salary: item.dataset.salary,
+            age: item.dataset.age
+        }));
+}
+sortList(mainList);
+getEmploees(mainList);
+
+//# sourceMappingURL=index.f75de5e1.js.map
